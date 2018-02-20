1ee3c40f3a4e135637a64c5ad1eae175a076405f
EvgeniiaD <dgenny.19.97@gmail.com>
2018-02-20 11:50:43 +0100

    add scripts

---
+++ Endless_Runner_2D/Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Hide();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Show()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Hide()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+}
