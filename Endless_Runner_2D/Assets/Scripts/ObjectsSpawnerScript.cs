2fb9ea830f5a1ec59e21725b6aa19b6fb0840729
xabi <xabi.bmx@gmail.com>
2018-02-20 21:06:18 +0100

    Add enemy array not completed

---
+++ Endless_Runner_2D/Assets/Scripts/ObjectsSpawnerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsSpawnerScript : MonoBehaviour
+{
+
+    public GameObject obstacles;
+    float rendX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            rendX = Random.Range(-3.2f, 3.2f);
+            whereToSpawn = new Vector2(rendX, transform.position.y);
+            Instantiate(obstacles, whereToSpawn, Quaternion.Identity);
+        }
+		
+	}
+}
