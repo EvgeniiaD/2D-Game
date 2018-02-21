21e079c78fd0e89cee4b0a5227deecc0d5792212
xabi <xabi.bmx@gmail.com>
2018-02-21 02:16:13 +0100

    Add Obstacles Behaviour and Spawn

--- Endless_Runner_2D/Assets/Scripts/ObjectsSpawnerScript.cs
+++ Endless_Runner_2D/Assets/Scripts/ObjectsSpawnerScript.cs
@@ -26,9 +26,9 @@ public class ObjectsSpawnerScript : MonoBehaviour
         if(Time.time > nextSpawn)
         {
             nextSpawn = Time.time + spawnRate;
-            rendX = Random.Range(-3.2f, 3.2f);
+            rendX = Random.Range(-3.1f, 3.1f);
             whereToSpawn = new Vector2(rendX, transform.position.y);
-            Instantiate(obstacles, whereToSpawn, Quaternion.Identity);
+            Instantiate(obstacles, whereToSpawn, Quaternion.identity);
         }
 		
 	}
