f695fbe644a730443790830d6498eb247e5b7fa7
xabi <xabi.bmx@gmail.com>
2018-02-21 02:14:47 +0100

    Obstacle behaviour

---
+++ Endless_Runner_2D/Assets/Scripts/ObstaclesBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesBehaviour : MonoBehaviour
+{
+    float maxSpeed = 2f;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 pos = transform.position;
+        pos.y +=  maxSpeed * Time.deltaTime;
+        transform.position = pos;
+	}
+} 
