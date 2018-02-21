d7172bc9c5b85abfbe0749060076a5c1f80289d4
xabi <xabi.bmx@gmail.com>
2018-02-21 02:12:23 +0100

    add scripts

---
+++ Endless_Runner_2D/Assets/Scripts/Destroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+    
+	public GameObject block;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            Destroy(other);
+        }
+    }
+
+
+}
